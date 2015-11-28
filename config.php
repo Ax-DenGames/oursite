fe6250626d06430382e35614efaf0209a65135e7
Ax-DenGames <axdengames@gmail.com>
2015-11-28 14:35:34 +0300

    Lets start write site
    At now moment i write first string of good PHP code. Lets code guys

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$title1="Our portfolio";
+$developers="";
+
+//databasedata
+$user="";
+$pass="";
+$dbname="";
+$host="";
+
+
+?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+include_once("config.php");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Our Site</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+         <link rel="stylesheet" href="css/bootstrap-theme.css">
+         <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="index.php">Techologes</a></li>
+                <li><a href="index.php">Our Work</a></li>
+                <li><a href="index.php">Team</a></li>
+                <li><a href="index.php">Blog</a></li>
+                
+            </ul>
+        </nav>
+        <script src="/js/bootstrap.js"/>
+        
+    </body>
+</html>
+        
+       
